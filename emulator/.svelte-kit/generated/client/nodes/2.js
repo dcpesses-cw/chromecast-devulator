ec3d5b0a5f77f2ec3be29014a261f84a918b13c4
Jaclyn Staples <jaclyn.staples@paramount.com>
2025-02-19 14:54:08 -0400

    switched package engine to pnpm and created an extension directory

---
+++ emulator/.svelte-kit/generated/client/nodes/2.js
@@ -0,0 +1,1 @@
+export { default as component } from "../../../../src/routes/sender/+layout.svelte";
---
+++ emulator/.svelte-kit/generated/client/nodes/4.js
@@ -0,0 +1,1 @@
+export { default as component } from "../../../../src/routes/receiver/+page.svelte";
